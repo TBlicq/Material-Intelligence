9462cdcba16b80a5eca510129719b7ee86708d03
TBlicq <boxelltoney@gmail.com>
2026-08-05 16:12:17 -0500

    Update reporting.js

--- prototype/js/reporting.js
+++ prototype/js/reporting.js
@@ -1,36 +1,68 @@
 async function loadImportData() {
 
     const response = await fetch("data/import-framing.json");
-
     const data = await response.json();
 
     generateBuildingReport(data.imports);
+    generateFloorReport(data.imports);
 }
 
 function generateBuildingReport(imports) {
 
-    const buildingTotals = {};
+    const totals = {};
 
     imports.forEach(item => {
 
-        if (!buildingTotals[item.building]) {
-
-            buildingTotals[item.building] = 0;
+        if (!totals[item.building]) {
+            totals[item.building] = 0;
         }
 
-        buildingTotals[item.building] += item.quantity;
+        totals[item.building] += item.quantity;
     });
 
-    const tbody = document.getElementById("building-report-body");
+    const tbody =
+        document.getElementById("building-report-body");
 
     tbody.innerHTML = "";
 
-    Object.keys(buildingTotals).forEach(building => {
+    Object.entries(totals).forEach(([building, qty]) => {
 
         tbody.innerHTML += `
             <tr>
                 <td>${building}</td>
-                <td>${buildingTotals[building]}</td>
+                <td>${qty}</td>
+            </tr>
+        `;
+    });
+}
+
+function generateFloorReport(imports) {
+
+    const totals = {};
+
+    imports.forEach(item => {
+
+        const key =
+            `${item.building}-${item.floor}`;
+
+        if (!totals[key]) {
+            totals[key] = 0;
+        }
+
+        totals[key] += item.quantity;
+    });
+
+    const tbody =
+        document.getElementById("floor-report-body");
+
+    tbody.innerHTML = "";
+
+    Object.entries(totals).forEach(([floor, qty]) => {
+
+        tbody.innerHTML += `
+            <tr>
+                <td>${floor}</td>
+                <td>${qty}</td>
             </tr>
         `;
     });
